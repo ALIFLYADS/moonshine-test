4b20e0895471eda9b4295e2f0389d7d81510763c
Opekunov <Opekunov@users.noreply.github.com>
2024-12-31 18:19:51 +0000

    Apply prettier changes

--- src/UI/resources/js/__tests__/Support/DispatchEvents.test.js
+++ src/UI/resources/js/__tests__/Support/DispatchEvents.test.js
@@ -1,8 +1,8 @@
-import { dispatchEvents } from '../../Support/DispatchEvents';
+import {dispatchEvents} from '../../Support/DispatchEvents'
 import {beforeEach, describe, expect, it, jest} from '@jest/globals'
 
 describe('dispatchEvents', () => {
-  let component;
+  let component
 
   beforeEach(() => {
     component = {
@@ -14,42 +14,42 @@ describe('dispatchEvents', () => {
         }),
       },
       $dispatch: jest.fn(),
-    };
-  });
+    }
+  })
 
   it('should return early if events is not provided', () => {
-    dispatchEvents(null, 'eventType', component);
-    expect(component.$dispatch).not.toHaveBeenCalled();
-  });
+    dispatchEvents(null, 'eventType', component)
+    expect(component.$dispatch).not.toHaveBeenCalled()
+  })
 
   it('should return early if events is not a string', () => {
-    dispatchEvents([], 'eventType', component);
-    expect(component.$dispatch).not.toHaveBeenCalled();
-  });
+    dispatchEvents([], 'eventType', component)
+    expect(component.$dispatch).not.toHaveBeenCalled()
+  })
 
   it('should replace {row-id} with rowKey from dataset', () => {
-    dispatchEvents('{row-id}', 'eventType', component);
-    expect(component.$dispatch).toHaveBeenCalledWith('123', {});
-  });
+    dispatchEvents('{row-id}', 'eventType', component)
+    expect(component.$dispatch).toHaveBeenCalledWith('123', {})
+  })
 
   it('should dispatch events correctly', () => {
-    const events = 'click|param1=value1;param2=value2,hover|param3=value3';
-    dispatchEvents(events, 'eventType', component, { extraParam: 'extraValue' });
+    const events = 'click|param1=value1;param2=value2,hover|param3=value3'
+    dispatchEvents(events, 'eventType', component, {extraParam: 'extraValue'})
 
-    expect(component.$dispatch).toHaveBeenCalledTimes(2);
+    expect(component.$dispatch).toHaveBeenCalledTimes(2)
     expect(component.$dispatch).toHaveBeenNthCalledWith(1, 'click', {
       extraParam: 'extraValue',
       param1: 'value1',
       param2: 'value2',
-    });
-    expect(component.$dispatch).toHaveBeenNthCalledWith(2,'hover', {
+    })
+    expect(component.$dispatch).toHaveBeenNthCalledWith(2, 'hover', {
       extraParam: 'extraValue',
       param3: 'value3',
-    });
-  });
+    })
+  })
 
   it('should not dispatch events if type is error', () => {
-    dispatchEvents('click', 'error', component);
-    expect(component.$dispatch).not.toHaveBeenCalled();
-  });
-});
+    dispatchEvents('click', 'error', component)
+    expect(component.$dispatch).not.toHaveBeenCalled()
+  })
+})
